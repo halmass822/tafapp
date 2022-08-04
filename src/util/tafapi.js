6dd38c5a4581e2c0555fd27d3db7b37c8a40a0b9
Hazem A <halmassri822@gmail.com>
2022-08-04 18:02:57 -0400

    syling and parsing changes

--- src/util/tafapi.js
+++ src/util/tafapi.js
@@ -18,12 +18,17 @@ const TAF = {
         const wind = inputArray.find((x) => {
             return (/KT/).test(x);
         })
+        const processedWind = `${wind.slice(0,3)}° ${wind.slice(3)}`
         const visibility = inputArray.find((x) => {
             return (/SM/).test(x);
         })
-        const cloud = inputArray.filter((x) => {
+        let cloud = inputArray.filter((x) => {
             return /(SKC)|(FEW)|(SCT)|(BKN)|(OVC)/.test(x);
         })
+        let processedCloud = [];
+        for(let i = 0; i < cloud.length ; i+=6){
+            processedCloud.push(`${cloud.slice(i,i+6)}`);
+        }
         const temp = inputArray.find((x) => {
             return (/\d\d\/\d\d/).test(x);
         })
@@ -33,16 +38,16 @@ const TAF = {
         return {
             icao:icao,
             time:time,
-            wind:wind,
+            wind:processedWind,
             visibility:visibility,
-            cloud:cloud,
+            cloud:processedCloud.join(),
             temp:temp,
             pressure:pressure
         }
     },
     parseStations(inputString) {
         let processedString = inputString.toUpperCase().replace(/[^A-Z]/g,"");
-        if(processedString.length%4 == 0 && processedString.length >= 4){
+        if(processedString.length%4 === 0 && processedString.length >= 4){
             const outputArray = [];
             for(let i = 0; i < (processedString.length) ; i +=4){
                 outputArray.push(processedString.substring(i,i+4));
