f0bd6e39998991c282d45516db1b619c5a9aa041
Hazem A <halmassri822@gmail.com>
2022-08-04 04:04:02 -0400

    added alt to the logos

--- src/components/Metar/Metar.js
+++ src/components/Metar/Metar.js
@@ -11,17 +11,13 @@ import wind from '../../tafappimg/wind.png';
 export class Metar extends React.Component {
     render() {
         return <div className="metarReport">
-            
-                <h2><img src={airport} className="metarLogo"></img>{this.props.icao}</h2>
-                <h2><img src={clock} className="metarLogo"></img>{this.props.time}</h2>
-                <h2><img src={wind} className="metarLogo"></img>{this.props.wind}</h2>
-                <h2><img src={binoculars} className="metarLogo"></img>{this.props.visibility}</h2>
-            
-            
-                <h2><img src={cloud} className="metarLogo"></img>{this.props.cloud}</h2>
-                <h2><img src={temp} className="metarLogo"></img>{this.props.temp}</h2>
-                <h2><img src={pressure} className="metarLogo"></img>{this.props.pressure}</h2>
-            
+            <h2><img src={airport} className="metarLogo" alt="airport logo"></img>{this.props.icao}</h2>
+            <h2><img src={clock} className="metarLogo" alt="clock logo"></img>{this.props.time}</h2>
+            <h2><img src={wind} className="metarLogo" alt="wind logo"></img>{this.props.wind}</h2>
+            <h2><img src={binoculars} className="metarLogo" alt="binoculars logo"></img>{this.props.visibility}</h2>
+            <h2><img src={cloud} className="metarLogo" alt="cloud logo"></img>{this.props.cloud}</h2>
+            <h2><img src={temp} className="metarLogo" alt="temp logo"></img>{this.props.temp}</h2>
+            <h2><img src={pressure} className="metarLogo" alt="pressure logo"></img>{this.props.pressure}</h2>
         </div>
     }
 }
