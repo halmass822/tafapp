5199750e08b181a0c3cdf1e4734292f5a1946a95
Hazem A <halmassri822@gmail.com>
2022-08-09 15:23:08 -0400

    fixed app.js

--- src/App.js
+++ src/App.js
@@ -12,21 +12,7 @@ export class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <div className="metarSection">
-          <h1 className="sectionTitle">METAR</h1>
-          {this.state.metars && this.state.metars.map((metar) => {
-            return <Metar
-              place={metar.place}
-              icao={metar.icao}
-              time={metar.time}
-              wind={metar.wind}
-              visibility={metar.visibility}
-              cloud={metar.cloud}
-              temp={metar.temp}
-              pressure={metar.pressure}
-            />
-          })}
-        </div>
+        <MetarReports />
       </div>
     );
   }
